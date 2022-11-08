d32a1bc9b6f9e0d03f41e863745078809e1c8329
Guillem Agulló <guillem.agullo@enti.cat>
2022-11-08 11:19:41 +0100

    Relacions de servidor client enviant memes

--- client.sh
+++ client.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
+IP_SERVER="localhost"
+IP_LOCAL="127.0.0.1"
+
+PORT="4242"
+
 echo "Cliente HMTP"
 
 echo "(1) SEND - Enviando el Handshake"
 
-echo "GREEN_POWA 127.0.0.1" | nc localhost 4242
+echo "GREEN_POWA $IP_LOCAL" | nc $IP_SERVER $PORT
 
 echo "(2) LISTEN - Escuchando confirmación"
 
-MSG=`nc -l 4242`
+MSG=`nc -l $PORT`
 
 echo "$MSG"
 
@@ -18,4 +23,32 @@ then
 	exit 1
 fi
 
-echo "seguimos"
+echo "(5) SEND - file_Name (enviem el nom del arxiu)"
+
+FILE_NAME="elon_musk.jpg"
+
+FIlE_MD5=`echo $FILE_NAME | md5sum | cut -d " " -f 1`
+
+echo "FILE_NAME $FILE_NAME $FILE_MD5" | nc $IP_SERVER $PORT
+
+echo "(6) LISTEN - Escoltant confirmació d'arxiu"
+MSG=`nc -l $PORT`
+if [ "$MSG" != "OK_FILE_NAME" ]
+then
+	echo "ERROR 2: Nombre de archivo enviado incorrectamente"
+	exit 2
+fi
+
+echo "(9) SEND - Enviamos los datos del archivo"
+
+cat memes/$FILE_NAME | nc $IP_SERVER $PORT
+
+echo "(10) LISTEN - Escuchamos confirmación de datos del archivo"
+
+MSG=`nc -l $PORT`
+
+if [ "$MSG" != "OK DATA_RCPT" ]
+then
+	echo 
+fi
+exit 0
--- server.sh
+++ server.sh
@@ -1,18 +1,49 @@
 #!/bin/bash
 
+PORT="4242"
+
 echo "Servidor HMTP"
 
-MSG=`nc -l 4242`
+echo "(0) Listen - Levantando servidor"
+
+MSG=`nc -l $PORT`
 
 HANDSHAKE=`echo $MSG | cut -d " " -f 1`
 IP_CLIENT=`echo $MSG | cut -d " " -f 2`
 
+echo "(3) Send - Confirmación Handshake"
+
 if [ "$HANDSHAKE" != "GREEN_POWA" ]
 then
-	echo "KO_HMTP" | nc $IP_CLIENT 4242
-else
-	echo "OK_HMTP" | nc $IP_CLIENT 4242
+	echo "KO_HMTP" | nc $IP_CLIENT $PORT
+	exit 1
 fi
 
+echo "OK_HMTP" | nc $IP_CLIENT $PORT
+echo "(4) Listen"
 
+MSG=`nc -l $PORT`
 
+echo "$MSG"
+PREFIX=`echo $MSG | cut -d " " -f 1`
+FILE_NAME=`echo $MSG | cut -d " " -f 2`
+FILE_MD5=`echo $MSG | cut -d " " -f 3`
+echo "(7) SEND - Confirmación del nombre del archivo"
+
+if [ $PREFIX != "FILE_NAME" ]
+then
+	echo "KO_HMTP" | nc $IP_CLIENT
+	exit 2
+fi
+
+echo "OK_FILE_NAME" | nc $IP_CLIENT $PORT
+
+echo "(8) LISTEN - Escuchando datos de archivo"
+
+nc -l $PORT > inbox/$FILE_NAME
+
+echo "(11) SEND - Confirmació recepció"
+
+echo "OK_DATA_RCPT" | nc $IP_CLIENT $PORT
+
+exit 0 
